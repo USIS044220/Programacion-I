5e037f65c9aceac43d233a9cdb23b67e305de016
Luis Enrique Hernandez <sarco_luis@hotmail.com>
2020-07-31 12:26:35 -0600

    Creacion del ejercicio de la tabla de multiplicar

--- holaMundo/Form1.vb
+++ holaMundo/Form1.vb
@@ -1,46 +1,15 @@
 ﻿Public Class Form1
-    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        'Dim es una palabra reservada para declarar una variable
-        Dim num1, num2, respuesta As Double 'Declaro las variables en linea separadas por comas del mismo tipo
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla, i As SByte
+        ntabla = txtntabla.Text
 
-        num1 = txtnum1.Text 'capturando el dato que ingres el usuario a la caja de texto, asigno dicho valor a la variable num1.
-        num2 = txtnum2.Text
-
-        If optSuma.Checked Then 'If -> si de condicion
-            lblrespuesta.Text = num1 + num2 'realizamos la suma
+        lstTablas.Items.Clear()
+        If ntabla <= 12 Then
+            For i = 1 To 10
+                lstTablas.Items.Add(ntabla.ToString() + "X" + i.ToString() + "=" + (ntabla * i).ToString())
+            Next
+        Else
+            lstTablas.Items.Add("Por favor ingrese solo tablas menores o iguales a 12.")
         End If
-
-        If optResta.Checked Then
-            lblrespuesta.Text = num1 - num2 'realizamos la resta
-        End If
-
-        If optMultiplicar.Checked Then
-            lblrespuesta.Text = num1 * num2 'realizamos la multiplicacion
-        End If
-
-        If optDividir.Checked Then
-            lblrespuesta.Text = num1 / num2 'realizamos la division
-        End If
-
-        If optPorcentaje.Checked Then
-            'Realizar el proceso de porcentaje....
-        End If
-        If optExponenciacion.Checked Then
-            'Realizar el proceso de exponenciacion....
-        End If
-        '...
-        Select Case cboOperaciones.SelectedIndex
-            Case 1 'Suma
-                lblrespuesta.Text = num1 + num2
-
-            Case 2 'Resta
-                lblrespuesta.Text = num1 - num2
-
-            Case 3 'Multiplicacion
-                lblrespuesta.Text = num1 * num2
-
-            Case 4 'Division
-                lblrespuesta.Text = num1 / num2
-        End Select
     End Sub
 End Class
